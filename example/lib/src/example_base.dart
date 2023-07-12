5175279f6e5afa5ecffae88fe4bb6cd2288ad016
M Atheer <55885230+masreplay@users.noreply.github.com>
2023-07-12 20:37:45 +0300

    fix:

--- example/lib/src/example_base.dart
+++ example/lib/src/example_base.dart
@@ -1,1 +1,25 @@
-/// read `example.dart`
+import 'package:example/src/service/clients/users_client.dart';
+import 'package:example/src/service/clients/stores_client.dart';
+import 'package:example/src/service/models/user.dart';
+import 'package:example/src/service/models/category.dart';
+import 'package:example/src/service/models/store.dart';
+import 'package:example/src/service/models/product.dart';
+
+void main(List<String> arguments) {
+  final usersClient = UsersClient();
+  final storesClient = StoresClient();
+
+  final user = User(name: "", age: 0);
+  final store = Store(name: "name", location: "location");
+  final category = Category(name: "name", icon: "icon");
+  final product = Product(name: "name", price: "price", color: "color");
+
+  print(category);
+  print(product);
+
+  usersClient.create(user);
+  usersClient.delete(user);
+
+  storesClient.create(store);
+  storesClient.update(store);
+}

74c0b967c38c6470f641285effc1c3477b270e92
M Atheer <55885230+masreplay@users.noreply.github.com>
2023-07-12 17:54:31 +0300

    fix

--- lib/exports.dart
+++ lib/exports.dart
@@ -2,10 +2,10 @@ import 'dart:io';
 
 import 'package:exports/export_yaml.dart';
 import 'package:glob/glob.dart';
-import 'package:path/path.dart' as path;
+import 'package:path/path.dart' as p;
 
 void main(List<String> arguments) {
-  exitCode = 0; // presume success
+  exitCode = 0;
 
   export();
 }
@@ -16,43 +16,50 @@ Future<void> export([String? filepath]) async {
       : ExportYaml.fromFile(filepath);
 
   print(content.exports);
+  print(content.ignores);
 
   final List<String> exports = content.exports;
   final List<String> ignores = content.ignores;
 
-  print('1 + 1 = ...');
-
   final ignoreGlobs = ignores.map((path) => Glob(path)).toList();
 
   for (var path in exports) {
-    final dir = Directory(path);
+    var dir = Directory(path);
+    createExportFilesRecursively(dir, ignoreGlobs);
+  }
+}
+
+void createExportFilesRecursively(Directory dir, List<Glob> ignoreGlobs) {
+  final dirName = p.basename(dir.path);
+
+  if (!['lib', 'src'].contains(dirName)) {
     createExportFile(dir, ignoreGlobs);
+  }
+
+  final subDirs = dir.listSync(recursive: false).whereType<Directory>();
+  for (var subDir in subDirs) {
+    createExportFilesRecursively(subDir, ignoreGlobs);
   }
 }
 
 void createExportFile(Directory dir, List<Glob> ignoreGlobs) {
-  // Recursively loop through the files and subdirectories
-  final files = dir.listSync(recursive: true);
+  final files = dir.listSync(recursive: false);
 
-  // Filter out ignored files and directories
   final filteredFiles = files.where((file) {
+    if (file is File && matchDirAndFileName(dir, file)) {
+      return false;
+    }
+
     return !ignoreGlobs.any((glob) => glob.matches(file.path));
   }).toList();
 
-  // Create new .dart file for exports
-  final exportFile =
-      File(path.join(dir.path, '${dir.path.split("/").last}.dart'));
-
-  print(exportFile.path);
-
+  final exportFile = File('${dir.path}/${dir.path.split("/").last}.dart');
   exportFile.createSync();
 
-  // Write the exports to the new file
   final sink = exportFile.openWrite();
 
   for (var entity in filteredFiles) {
-    final path =
-        entity.path.replaceFirst('${dir.path}/', ''); // Get relative path
+    final path = entity.path.replaceFirst('${dir.path}/', '');
 
     if (entity is File) {
       sink.write('export "$path";\n');
@@ -61,6 +68,9 @@ void createExportFile(Directory dir, List<Glob> ignoreGlobs) {
     }
   }
 
-  // Close the IOSink to free system resources.
   sink.close();
+}
+
+bool matchDirAndFileName(Directory dir, File file) {
+  return file.path.split('/').last.split('.').first == dir.path.split('/').last;
 }
